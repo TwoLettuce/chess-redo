1542764ae9129b00d4ce4a37332ca0958cb6aa8b
TwoLettuce <grantharris0323@gmail.com>
2026-01-07 17:48:02 -0700

    Knight moves pass

--- shared/src/main/java/chess/moves/KnightMovesCalculator.java
+++ shared/src/main/java/chess/moves/KnightMovesCalculator.java
@@ -1,15 +1,38 @@
 package chess.moves;
 
 import chess.ChessBoard;
+import chess.ChessGame;
 import chess.ChessMove;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class KnightMovesCalculator extends MovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition pos) {
-        return List.of();
+        ArrayList<ChessMove> legalMoves = new ArrayList<>();
+        int row = pos.getRow();
+        int col = pos.getColumn();
+        ChessGame.TeamColor color = board.getPiece(pos).getTeamColor();
+
+        for (int rowOffset : new int[]{-2, -1, 1, 2}){
+            for (int colOffset : new int[]{-2, -1, 1, 2}){
+                if (Math.abs(rowOffset) != Math.abs(colOffset)){
+                    if (!isInBounds(row+rowOffset, col+colOffset)){
+                        continue;
+                    }
+                    ChessPosition potentialPos = new ChessPosition(row + rowOffset, col + colOffset);
+                    if (isBlocked(board, potentialPos, color)){
+                        continue;
+                    } else {
+                        legalMoves.add(new ChessMove(pos, potentialPos, null));
+                    }
+
+                }
+            }
+        }
+        return legalMoves;
     }
 }
