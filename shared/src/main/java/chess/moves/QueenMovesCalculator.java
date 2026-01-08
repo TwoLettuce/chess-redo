50cc04c132138c23b0196b8167733b1e1132046a
TwoLettuce <grantharris0323@gmail.com>
2026-01-07 17:58:14 -0700

    queen moves pass

--- shared/src/main/java/chess/moves/QueenMovesCalculator.java
+++ shared/src/main/java/chess/moves/QueenMovesCalculator.java
@@ -4,12 +4,17 @@ import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class QueenMovesCalculator extends MovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition pos) {
-        return List.of();
+        Collection<ChessMove> diagonals = new BishopMovesCalculator().pieceMoves(board, pos);
+        Collection<ChessMove> cardinals = new RookMovesCalculator().pieceMoves(board, pos);
+        Collection<ChessMove> legalMoves = diagonals;
+        legalMoves.addAll(cardinals);
+        return legalMoves;
     }
 }
