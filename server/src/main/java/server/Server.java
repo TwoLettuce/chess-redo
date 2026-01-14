b44c66d010399c5a286d72cd794c80ca4fb45576
TwoLettuce <grantharris0323@gmail.com>
2026-01-14 15:51:27 -0700

    basic server architecture

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,8 @@
 package server;
 
 import io.javalin.*;
+import io.javalin.http.Context;
+
 
 public class Server {
 
@@ -11,6 +13,39 @@ public class Server {
 
         // Register your endpoints and exception handlers here.
 
+        javalin.create().delete("/db", this::clear)
+                .post("/user", this::register)
+                .post("/session", this::login)
+                .delete("/session", this::logout)
+                .get("/game", this::list)
+                .post("/game", this::create)
+                .put("/game", this::join);
+
+
+    }
+
+    private void clear(Context ctx){
+
+    }
+
+    private void register(Context ctx){
+
+    }
+
+    private void login(Context ctx){
+
+    }
+
+    private void list(Context ctx){
+
+    }
+
+    private void create(Context ctx){
+
+    }
+
+    private void join(Context ctx){
+        
     }
 
     public int run(int desiredPort) {
